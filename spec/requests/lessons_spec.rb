522c9c1c422113b89ce35f41fe5f150f46715c7e
mkaszubowski <kaszuba44@gmail.com>
2014-07-29 23:42:06 +0200

    new lesson form tests

--- spec/requests/lessons_spec.rb
+++ spec/requests/lessons_spec.rb
@@ -8,5 +8,36 @@ describe "Lessons" do
 		before { visit new_subject_lesson_path(subject1) }
 		it { should have_content "Nowa lekcja" }
 		it { should have_selector "form" }
+
+		describe "it should create a new lesson" do
+			before do
+				@lesson = subject1.lessons.create(name: "lesson", 
+					description: "lorem", video_link: "youtube.com/watch?v=f4")
+				fill_in "Nazwa", with: @lesson.name
+				fill_in "Opis", with: @lesson.description
+				fill_in "Adres filmu", with: @lesson.video_link
+
+				click_button "Dodaj lekcję"
+			end
+
+			it { should have_content "Dodano lekcję" }
+			it { should have_content @lesson.name }
+		end
+
+		describe "for blank data" do
+			before { click_button "Dodaj lekcję" }
+
+			it { should have_content "Wystąpiły błędy w formularzu." }
+		end
+
+		describe "for invalid video link" do
+			before do
+				fill_in "Adres filmu", with: "lorem"
+				click_button "Dodaj lekcję"
+			end
+
+			it { should have_content "Wystąpiły błędy w formularzu" }
+			it { should have_content "Niepoprawny format" }
+		end
 	end
 end
