db638e8ec2d488d8abdc2599d15d668330e245da
mkaszubowski <maciej@mkaszubowski.pl>
2014-09-19 13:05:15 +0200

    deleting subjects from admin panel

--- app/controllers/admin/subjects_controller.rb
+++ app/controllers/admin/subjects_controller.rb
@@ -5,8 +5,20 @@ class Admin::SubjectsController < ApplicationController
   layout 'admin'
 
   before_action :authenticate_admin
-  
+
   def index
     @subjects = Subject.all
+  end
+
+  def destroy
+    @subject = Subject.find(params[:id])
+
+    if @subject.destroy
+      flash[:success] = "Usunięto przedmiot"
+      redirect_to :back
+    else 
+      flash[:error] = "Wystąpił błąd, spróbuj ponownie"
+    end
+
   end
 end
