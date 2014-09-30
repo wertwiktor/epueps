b4b214285e5c7104a7ed2a9bc0b722083163b921
mkaszubowski <maciej@mkaszubowski.pl>
2014-09-30 13:51:25 +0200

    DRY the admin/video forms

--- app/helpers/admin/videos_helper.rb
+++ app/helpers/admin/videos_helper.rb
@@ -1,2 +1,19 @@
 module Admin::VideosHelper
+
+  def video_form_url(subject, lesson, video)
+    if video.new_record?
+      admin_subject_lesson_videos_path(subject, lesson, video)
+    else
+      admin_subject_lesson_video_path(subject, lesson, video)
+    end
+  end
+
+
+  def video_form_submit(video)
+    if video.new_record?
+      "Dodaj film"
+    else
+      "Zapisz zmiany"
+    end
+  end
 end
