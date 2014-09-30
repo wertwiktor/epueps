c52fc96f07ea397a61c093ed40f226d7a6a24e92
mkaszubowski <maciej@mkaszubowski.pl>
2014-09-30 13:38:12 +0200

    DRY the admin/subject forms

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -1,2 +1,20 @@
 module SubjectsHelper
+
+  def subject_form_url(subject)
+    if subject.new_record?
+      admin_subjects_path(subject)
+    else
+      admin_subject_path(subject)
+    end
+  end
+
+
+  def subject_form_submit(subject)
+    if subject.new_record?
+      "Dodaj przedmiot"
+    else
+      "Zapisz zmiany"
+    end
+  end
+
 end
