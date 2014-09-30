333667e6a009f1563128f4db4536e92f25df1140
mkaszubowski <maciej@mkaszubowski.pl>
2014-09-30 13:45:45 +0200

    DRY the admin/lesson forms

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,2 +1,20 @@
 module LessonsHelper
+
+  def lesson_form_url(subject, lesson)
+    if lesson.new_record?
+      admin_subject_lessons_path(subject, lesson)
+    else
+      admin_subject_lesson_path(subject, lesson)
+    end
+  end
+
+
+  def lesson_form_submit(lesson)
+    if lesson.new_record?
+      "Dodaj lekcję"
+    else
+      "Zapisz zmiany"
+    end
+  end
+
 end
