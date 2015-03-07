caaab6eddab4942cb0db2e9d9bfb3488e44c1c9d
wertwiktor <wertwiktor@users.noreply.github.com>
2015-03-07 19:29:56 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,15 +35,21 @@ set :scm, :git
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
 
 end
